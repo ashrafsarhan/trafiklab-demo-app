0e01ca4d71737d4009b195137c8bf49988aa18e3
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-04-30 17:26:13 +0200

    first commit

---
+++ src/test/java/com/trafiklab/TrafiklabDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafiklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafiklabDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
