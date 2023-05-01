1bec8c6ac99d64d4465b10d4954c0e97d6eed72e
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-05-01 22:10:20 +0200

    Refactoring StopsLinesService - Validate lineDataResponse based on statusCode - Utilizing MapUtil class

--- src/main/java/com/trafiklab/service/StopsLinesService.java
+++ src/main/java/com/trafiklab/service/StopsLinesService.java
@@ -3,25 +3,26 @@ package com.trafiklab.service;
 import com.trafiklab.api.SlService;
 import com.trafiklab.dto.JourneyPatternPointOnLine;
 import com.trafiklab.dto.LineDataResponse;
+import com.trafiklab.utils.MapUtil;
 import com.trafiklab.utils.RestClient;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StopsLinesService implements SlService {
 
     private final RestClient<LineDataResponse> restClient;
+
+    private final MapUtil<String, JourneyPatternPointOnLine> mapUtil;
 
     @Value("${lineDataApiUrl}")
     private String lineDataApiUrl;
@@ -36,13 +37,10 @@ public class StopsLinesService implements SlService {
         Optional<LineDataResponse> lineDataResponse = restClient
                 .executeGetApiCallRetryable(String.format("%s?key=%s&model=jour&DefaultTransportModeCode=BUS", lineDataApiUrl,lineDataApiKey),
                         LineDataResponse.class);
-        return lineDataResponse.get().responseData.getResult()
-                .parallelStream().collect(Collectors.groupingBy(JourneyPatternPointOnLine::getLineNumber))
-                .entrySet()
-                .parallelStream()
-                .sorted(Comparator.comparing(l -> l.getValue().size(), Comparator.reverseOrder()))
-                .limit(listSize)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        if(lineDataResponse.isPresent() && lineDataResponse.get().statusCode == 0)
+            return mapUtil.getFirstEntries(mapUtil.sortMapBasedOnListValueSize(lineDataResponse.get().responseData.getResult()
+                    .stream().collect(Collectors.groupingBy(JourneyPatternPointOnLine::getLineNumber))), listSize);
+        log.warn("Api call response with status code:{} does not have the expected data: {}", lineDataResponse.get().statusCode, lineDataResponse);
+        return new HashMap<>();
     }
 }
