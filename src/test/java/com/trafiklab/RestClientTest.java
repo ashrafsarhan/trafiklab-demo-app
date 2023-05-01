43be156ecc031b4f728d17b589ec5728edc0a45e
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-05-01 17:46:01 +0200

    Create RestClient unit test

---
+++ src/test/java/com/trafiklab/RestClientTest.java
@@ -0,0 +1,36 @@
+package com.trafiklab;
+
+import com.trafiklab.dto.LineDataResponse;
+import com.trafiklab.utils.RestClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class RestClientTest {
+
+	@Autowired
+	private RestClient<LineDataResponse> restClient;
+
+	@Test
+	void testLineDataResponseRestClientSuccess() {
+		Optional<LineDataResponse> lineDataResponse = restClient
+				.executeGetApiCallRetryable("https://api.sl.se/api2/linedata.json?key=74dc8f1ae23143858613b2dab98fee77&model=jour&DefaultTransportModeCode=BUS",
+						LineDataResponse.class);
+		Assertions.assertNotNull(lineDataResponse.get(), "Test LineDataResponseRestClient Fail ");
+	}
+
+	@Test
+	void testLineDataResponseRestClientFail() {
+		Assertions.assertThrows(RuntimeException.class, () -> {
+		restClient
+				.executeGetApiCallRetryable("https://api.sl.se/api/linedata.json?key=74dc8f1ae23143858613b2dab98fee77&model=jour&DefaultTransportModeCode=BUS",
+						LineDataResponse.class);
+				}
+		, "Test LineDataResponseRestClient Fail");
+	}
+
+}
