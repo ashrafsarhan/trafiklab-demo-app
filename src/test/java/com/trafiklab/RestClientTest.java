ecd3675631532fa4bfc8eed05ade0dd817fdcd4d
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-05-01 22:30:06 +0200

    Refactoring RestClient unit test

--- src/test/java/com/trafiklab/RestClientTest.java
+++ src/test/java/com/trafiklab/RestClientTest.java
@@ -5,33 +5,41 @@ import com.trafiklab.utils.RestClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
+
 @ActiveProfiles("test")
 @SpringBootTest
 class RestClientTest {
 
-	@Autowired
-	private RestClient<LineDataResponse> restClient;
+    @Autowired
+    private RestClient<LineDataResponse> restClient;
 
-	@Test
-	void testLineDataResponseRestClientSuccess() {
-		Optional<LineDataResponse> lineDataResponse = restClient
-				.executeGetApiCallRetryable("https://api.sl.se/api2/linedata.json?key=74dc8f1ae23143858613b2dab98fee77&model=jour&DefaultTransportModeCode=BUS",
-						LineDataResponse.class);
-		Assertions.assertNotNull(lineDataResponse.get(), "Test LineDataResponseRestClient Fail ");
-	}
+    @Value("${lineDataApiUrl}")
+    private String lineDataApiUrl;
 
-	@Test
-	void testLineDataResponseRestClientFail() {
-		Assertions.assertThrows(RuntimeException.class, () -> {
-		restClient
-				.executeGetApiCallRetryable("https://api.sl.se/api/linedata.json?key=74dc8f1ae23143858613b2dab98fee77&model=jour&DefaultTransportModeCode=BUS",
-						LineDataResponse.class);
-				}
-		, "Test LineDataResponseRestClient Fail");
-	}
+    @Value("${lineDataApiKey}")
+    private String lineDataApiKey;
+
+    @Test
+    void testLineDataResponseRestClientSuccess() {
+        Optional<LineDataResponse> lineDataResponse = restClient
+                .executeGetApiCallRetryable(String.format("%s?key=%s&model=jour&DefaultTransportModeCode=BUS", lineDataApiUrl, lineDataApiKey),
+                        LineDataResponse.class);
+        Assertions.assertNotNull(lineDataResponse.get(), "Test LineDataResponseRestClient Fail ");
+    }
+
+    @Test
+    void testLineDataResponseRestClientFail() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+                    restClient
+                            .executeGetApiCallRetryable(String.format("https://api.sl.se/api/linedata.json?key=%s&model=jour&DefaultTransportModeCode=BUS", lineDataApiKey),
+                                    LineDataResponse.class);
+                }
+                , "Test LineDataResponseRestClient Fail");
+    }
 
 }
