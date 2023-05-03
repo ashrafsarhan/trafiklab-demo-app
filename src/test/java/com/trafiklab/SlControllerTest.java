a25d0a1572e19e4725a8e6b5b7d02ad8cd4c7648
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-05-04 00:09:43 +0200

    Refactor rest layer unit test

--- src/test/java/com/trafiklab/SlControllerTest.java
+++ src/test/java/com/trafiklab/SlControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,11 +21,21 @@ public class SlControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetTopListOfBusLinesWithMostStopsSuccess() throws Exception {
-        this.mockMvc.perform(get("/SL/TopListOfBusLinesWithMostStops/10"))
+    public void testGetTopListOfBusLinesWithMostStopsStreamImplWithDefaultRequestParamsSuccess() throws Exception {
+        this.mockMvc.perform(get("/SL/TopListOfBusLinesWithMostStops/StreamImpl"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].lineNumber", is("636")))
-                .andExpect(jsonPath("$[1].lineNumber", is("626")));
+                .andExpect(jsonPath("$[9].lineNumber", is("783")));
+    }
+
+    @Test
+    public void testGetTopListOfBusLinesWithMostStopsPlainImplWithGivenRequestParamsSuccess() throws Exception {
+        this.mockMvc.perform(get("/SL/TopListOfBusLinesWithMostStops/PlainImpl")
+                        .param("listSize", "5")
+                        .param("sortOrder", "ASC"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].stopsCount", is(4)))
+                .andExpect(jsonPath("$[4].stopsCount", is(7)));
     }
 
 }
