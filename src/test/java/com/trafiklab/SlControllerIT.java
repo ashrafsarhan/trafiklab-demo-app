e6b7cddfa9c453c1404c65abd0de622d2511c963
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2023-05-04 16:00:50 +0200

    Add integration tests suffix (IT) to SlController test

--- src/test/java/com/trafiklab/SlControllerIT.java
+++ src/test/java/com/trafiklab/SlControllerIT.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SlControllerTest {
+public class SlControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
